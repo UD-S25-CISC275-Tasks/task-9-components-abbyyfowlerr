17df3f1777b41096d7a1d265ac28407ee7cfca77
Abby <agfowler@udel.edu>
2025-02-23 14:54:14 -0500

    trying to fix linter react-dom error

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 // You will not need to modify this file
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// Create root using ReactDOM.createRoot
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
